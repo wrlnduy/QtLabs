74bbeb0d79af3bbc12663f849086977e2e5d6054
wrlnduy <90478427+wrlnduy@users.noreply.github.com>
2025-04-01 15:40:25 +0300

    Add Polygon

--- labs/raycaster/src/Polygon.cpp
+++ labs/raycaster/src/Polygon.cpp
@@ -1,5 +1,87 @@
-//
-// Created by wrlnduy on 30.03.25.
-//
-
 #include "Polygon.h"
+
+#include "Ray.h"
+#include "utils.h"
+
+#include <QPointF>
+#include <cmath>
+#include <cstddef>
+#include <limits>
+#include <optional>
+#include <vector>
+
+Polygon::Polygon(const std::vector<QPointF>& vertices) : vertices_(vertices) {
+}
+
+std::vector<QPointF> Polygon::GetVertices() const {
+    return vertices_;
+}
+
+QPointF Polygon::GetVertex(const size_t& ind) const {
+    if (ind == vertices_.size()) {
+        return vertices_[0];
+    }
+    return vertices_.at(ind);
+}
+
+void Polygon::AddVertex(const QPointF& vertex) {
+    vertices_.emplace_back(vertex);
+}
+
+void Polygon::UpdateLastVertex(const QPointF& vertex) {
+    if (!vertices_.empty()) {
+        vertices_.back() = vertex;
+    }
+}
+
+std::optional<QPointF> Polygon::IntersectRay(const Ray& ray) {
+    QPointF closest;
+    double min_dist = std::numeric_limits<double>::max();
+    bool found = false;
+
+    const QPointF ray_begin = ray.GetBegin();
+    const QPointF ray_dir = ray.GetEnd() - ray_begin;
+
+    for (size_t i = 0; i < vertices_.size(); ++i) {
+        const QPointF v1 = GetVertex(i);
+        const QPointF v2 = GetVertex(i + 1);
+
+        auto intersection = findIntersection(ray_begin, ray_dir, v1, v2);
+        if (intersection.has_value()) {
+            const double dist = std::hypot(
+                intersection.value().x() - ray.GetBegin().x(),
+                intersection.value().y() - ray.GetBegin().y());
+            if (utils::isLess(dist, min_dist)) {
+                min_dist = dist;
+                closest = intersection.value();
+                found = true;
+            }
+        }
+    }
+
+    if (found) {
+        return closest;
+    }
+
+    return std::nullopt;
+}
+
+std::optional<QPointF> Polygon::findIntersection(
+    const QPointF& ray_begin, const QPointF& ray_dir, const QPointF& edge_begin,
+    const QPointF& edge_end) {
+    const QPointF edge_dir = edge_end - edge_begin;
+    const double det = (ray_dir.x() * edge_dir.y()) - (ray_dir.y() * edge_dir.x());
+
+    if (utils::isEqual(det, .0)) {
+        return std::nullopt;
+    }
+
+    const QPointF diff = edge_begin - ray_begin;
+    const double t = (diff.x() * edge_dir.y() - diff.y() * edge_dir.x()) / det;
+    const double s = (diff.x() * ray_dir.y() - diff.y() * ray_dir.x()) / det;
+
+    if (!utils::isLess(t, .0) && !utils::isLess(s, .0) && !utils::isMore(s, 1.)) {
+        return ray_begin + ray_dir * t;
+    }
+    return std::nullopt;
+}
--- labs/raycaster/src/Polygon.h
+++ labs/raycaster/src/Polygon.h
@@ -1,16 +1,33 @@
-//
-// Created by wrlnduy on 30.03.25.
-//
-
 #ifndef POLYGON_H
 #define POLYGON_H
 
+#include "Ray.h"
 
+#include <QPointF>
+#include <cstddef>
+#include <optional>
+#include <vector>
 
 class Polygon {
+   public:
+    Polygon() = default;
+    explicit Polygon(const std::vector<QPointF>&);
 
+    [[nodiscard]] std::vector<QPointF> GetVertices() const;
+
+    [[nodiscard]] QPointF GetVertex(const size_t&) const;
+
+    void AddVertex(const QPointF&);
+
+    void UpdateLastVertex(const QPointF&);
+
+    [[nodiscard]] std::optional<QPointF> IntersectRay(const Ray&);
+
+   private:
+    std::vector<QPointF> vertices_{};
+
+    std::optional<QPointF> findIntersection(
+        const QPointF&, const QPointF&, const QPointF&, const QPointF&);
 };
 
-
-
-#endif //POLYGON_H
+#endif  // POLYGON_H
